498c73ab8d4b1e2f2b65f855e679e706e42ba456
Praveen Rahul <praveen.rahul@epicor.com>
2025-05-05 15:35:44 +0530

    Test Factory service locally

--- Design_Patterns/Creational_Design_Patterns/Factory_Design_Pattern/Code/payment/service.py
+++ Design_Patterns/Creational_Design_Patterns/Factory_Design_Pattern/Code/payment/service.py
@@ -1,4 +1,5 @@
 from gateways.base import PaymentGatewayAdapter, GiftCardCapable
+from factory import PaymentGatewayFactory
 
 class PaymentService:
     def __init__(self, gateway: PaymentGatewayAdapter):
@@ -11,3 +12,12 @@ class PaymentService:
         if isinstance(self.gateway, GiftCardCapable):
             return self.gateway.process_gift_card(gift_card_code, amount)
         raise NotImplementedError("Gift card processing not supported")
+
+if __name__ == '__main__':
+    gateway_type = PaymentGatewayFactory.get_gateway("stripe")
+    print(gateway_type.__dir__())
+    service = PaymentService(gateway_type)
+    gift_card_status = service.process_gift_card_payment("GC123", 50.0)
+    print(gift_card_status)
+    result = service.make_payment(100.0, "USD", {"card": "4242"})
+    print(result)
